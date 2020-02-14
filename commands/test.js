e9e90a48ddcb9a1b07556a2c2b1ac7effbbc0763
Glitch (alice-nww-bot) <none>
2020-02-14 13:44:07 +0000

    A Really Big Update

--- commands/test.js
+++ commands/test.js
@@ -45,6 +45,7 @@ module.exports = {
   name: "test",
   description: "For test, really.",
   async execute(message, args, pool) {
-    console.log(await getData("https://open.spotify.com/playlist/2yR017eSOXqWOsE8MpoJuJ?si=yJrujTf1Sb2NSYcNkY5VFg"))
+    var fetched = await message.channel.messages.fetch({limit: 2});
+    console.log(fetched);
   }
 };
